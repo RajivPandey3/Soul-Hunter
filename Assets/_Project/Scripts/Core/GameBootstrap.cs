f9c46dd0e1e9ddfd13630f5f3c4d3a19916a65f6
RAJIV PANDEY <rajiv8575@gmail.com>
2026-07-26 08:29:19 +0530

    First Backup Point

---
+++ Assets/_Project/Scripts/Core/GameBootstrap.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// Main entry point of the game framework.
+/// Responsible for initializing core systems.
+/// </summary>
+public class GameBootstrap : MonoBehaviour
+{
+    private void Awake()
+    {
+        InitializeFramework();
+    }
+
+
+    private void InitializeFramework()
+    {
+        Debug.Log("Soul Hunter Framework Initialized");
+    }
+}
